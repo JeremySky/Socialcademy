feafc66e62e893101515db517da5fc6e23bff4ae
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2023-05-29 13:45:30 -0700

    Initial Commit

---
+++ Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by Jeremy Manlangit on 5/29/23.
+//
+
+import SwiftUI
+
+@main
+struct SocialcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
